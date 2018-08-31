9fc8add15363b7b11d98b617eab3e5aba42d8e0e
LandSprutte <Christianbondesen@gmail.com>
2018-08-31 14:53:13 +0200

    controller for adding and fetching exercises from db

---
+++ src/db/models/exercise.model.ts
@@ -0,0 +1,18 @@
+import { Schema, Document } from 'mongoose';
+import mongoose from '../connection';
+
+const exerciseSchema = new Schema({
+    name: {type: String, required: true},
+    description: {type: String, required: true, maxlength: 1200},
+    repetition: {type: String, required: true}
+})
+
+export interface Exercise {
+    name: string;
+    description: string;
+    repetition: string;
+}
+
+export interface ExerciseModel extends Document, Exercise {};
+
+export default mongoose.model<ExerciseModel>('Exercise', exerciseSchema);
--- src/routes/fitness.ts
+++ src/routes/fitness.ts
@@ -1,5 +1,7 @@
 import { Router, Request, Response } from "express";
 import { data } from "../ts/formSubmit";
+import exerciseController from '../controllers/fitnessController';
+import { ExerciseModel } from "../db/models/exercise.model";
 
 export class Fitness {
   constructor(private router: Router) {
@@ -10,16 +12,24 @@ export class Fitness {
     return this.router;
   }
 
-  mainView(request: Request, response: Response) {
-    response.render("mainView", { name: "Yeah boi", fitnessItems: data });
+  mainView = async (request: Request, response: Response) => {
+    const {exercises} = await  exerciseController.getExercises();
+    response.render("mainView", { name: "Yeah boi", fitnessItems: exercises });
   }
 
-  submit(request: Request, response: Response) {
-    console.log(request.body());
+  submit(request: any, response: Response) {
+    exerciseController.addOneExercise(request.body)
+      .then(res => {
+      if (res) {
+        response.status(200).json(res.message);
+      } else {
+        response.status(404).json({message: "could not save"});
+      }
+    })
   }
 
   initRoutes() {
-    this.router.use("/submit", this.submit);
+    this.router.post("/submit", this.submit);
     this.router.use("/", this.mainView);
   }
 }
