e6ba56e7e4ffa34fb3c7fe27bd5a9af2d63cf36e
LandSprutte <Christianbondesen@gmail.com>
2018-09-04 13:49:03 +0200

    and this :)

---
+++ src/db/models/program.model.ts
@@ -0,0 +1,17 @@
+import { Schema, Document } from 'mongoose';
+import mongoose from '../connection';
+import { exerciseSchema, ExerciseModel } from './exercise.model';
+
+const programSchema = new Schema({
+  name: { type: String, required: true },
+  exercises: { type: [exerciseSchema]}
+});
+
+export interface Program {
+  name: string;
+  exercises: ExerciseModel[];
+}
+
+export interface ProgramModel extends Document, Program {}
+
+export default mongoose.model<ProgramModel>('Program', programSchema);
